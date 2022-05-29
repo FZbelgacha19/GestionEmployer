5a7ac1c563232ab2bf521a9ca81c1bfa0c43d406
FZbelgacha19 <fatimazahra.belgacha@gmail.com>
2022-05-29 14:20:12 +0100

    commit Blank Spring Project

---
+++ gestionempoyes/src/main/java/com/app/gestionempoyes/GestionempoyesApplication.java
@@ -0,0 +1,13 @@
+package com.app.gestionempoyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionempoyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionempoyesApplication.class, args);
+	}
+
+}
---
+++ gestionempoyes/src/test/java/com/app/gestionempoyes/GestionempoyesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gestionempoyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionempoyesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
