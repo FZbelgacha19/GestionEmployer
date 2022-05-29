1dfdbcb03909fc5b5318f29f782c4534e0192a47
FZbelgacha19 <fatimazahra.belgacha@gmail.com>
2022-05-29 15:39:58 +0100

    commit

--- backend/src/main/java/com/app/gestimemployes/BackendApplication.java
+++ backend/src/main/java/com/app/gestimemployes/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.app.gestionempoyes;
+package com.app.gestimemployes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionempoyesApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionempoyesApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/app/gestimemployes/BackendApplicationTests.java
+++ backend/src/test/java/com/app/gestimemployes/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.app.gestionempoyes;
+package com.app.gestimemployes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionempoyesApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
